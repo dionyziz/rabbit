679953d3aab6a5b3f1cdbcb56c5c12e0d74d778b
Aristotelis Mikropoulos <amikrop@gmail.com>
2008-07-25 15:55:27 +0000

    .

--- etc/rabbit.compile.php
+++ etc/rabbit.compile.php
@@ -9,39 +9,44 @@
     
     foreach ( $files as $file ) {
         assert( is_string( $file ) );
-        if ( empty( $file ) || substr( $file, 0, 1 ) != '/' ) {
-            continue;
-        }
-        $file = substr( $file, 1 );
-        if ( substr( $file, -1 ) == '~' ) {
-            if ( !file_exists( DESTINATION . $file ) ) {
-                echo "Warning: File $file does not exist in destination directory\n";
+        if ( !empty( $file ) && substr( $file, 0, 1 ) == '/' ) {
+            $file = substr( $file, 1 );
+            if ( substr( $file, -1 ) == '~' ) {
+                if ( !file_exists( DESTINATION . $file ) ) {
+                    echo "Warning: File $file does not exist in destination directory\n";
+                }
+                echo "Skipping: $file\n";
             }
-            echo "Skipping: $file\n";
-            continue;
+            else {
+                if ( substr( $file, -2 ) == '/*' ) {
+                    $file = substr( $file, 0, -2 );
+                    // echo 'Recursively copying ' . SOURCE . $file . ' to ' . DESTINATION . $file . "\n";
+                    $cmd = 'svn merge ' . escapeshellarg( DESTINATION . $file ) . '@HEAD ' . escapeshellarg( SOURCE . $file ) . '@HEAD ' . escapeshellarg( DESTINATION . $file );
+                    echo( "$cmd\n" );
+                    passthru( $cmd );
+                    ++$i;
+                }
+                else {
+                    if ( !file_exists( SOURCE . $file ) ) {
+                        echo "Warning: File $file does not exist in source location; skipping\n";
+                    }
+                    else {
+                        if ( is_dir( SOURCE . $file ) ) {
+                            echo "Skipping directory: $file\n";
+                        }
+                        else {
+                            $target = dirname( DESTINATION . $file );
+                            // echo 'Copying ' . SOURCE . $file . ' to ' . $target . "\n";
+                            $cmd = 'cp ' . escapeshellarg( SOURCE . $file ) . ' ' . escapeshellarg( $target );
+                            echo( "$cmd\n" );
+                            passthru( $cmd );
+                            passthru( 'svn add ' . escapeshellarg( DESTINATION . $file ) . ' --quiet' );
+                            ++$i;
+                        }
+                    }
+                }
+            }
         }
-        if ( substr( $file, -2 ) == '/*' ) {
-            $file = substr( $file, 0, -2 );
-            // echo "Recursively copying " . SOURCE . $file . " to " . DESTINATION . $file . "\n";
-            echo( 'svn merge ' . escapeshellarg( DESTINATION . $file ) . "@HEAD " . escapeshellarg( SOURCE . $file ) . "@HEAD " . escapeshellarg( DESTINATION . $file ) . "\n" );
-            passthru( 'svn merge ' . escapeshellarg( DESTINATION . $file ) . "@HEAD " . escapeshellarg( SOURCE . $file ) . "@HEAD " . escapeshellarg( DESTINATION . $file ) );
-            ++$i;
-            continue;
-        }
-        if ( !file_exists( SOURCE . $file ) ) {
-            echo "Warning: File $file does not exist in source location; skipping\n";
-            continue;
-        }
-        if ( is_dir( SOURCE . $file ) ) {
-            echo "Skipping directory: $file\n";
-            continue;
-        }
-        $target = dirname( DESTINATION . $file );
-        // echo "Copying " . SOURCE . $file . " to " . $target . "\n";
-        echo( "cp " . escapeshellarg( SOURCE . $file ) . ' ' . escapeshellarg( $target ) . "\n" );
-        passthru( "cp " . escapeshellarg( SOURCE . $file ) . ' ' . escapeshellarg( $target ) );
-        passthru( "svn add " . escapeshellarg( DESTINATION . $file ) . " --quiet" );
-        ++$i;
     }
 
     echo "$i sources copied\n";

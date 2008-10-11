0698496cbbebbdd44b0b99eacb8c18917bb05d28
Dionysis Zindros <dionyziz@gmail.com>
2008-10-11 14:54:08 +0000

    Coala console rendered redundant as of the new water debugger

--- elements/404.php
+++ elements/404.php
@@ -1,6 +1,7 @@
 <?php
     class Element404 extends Element {
         public function Render() {
+            header( 'HTTP/1.0 404 Not Found' );
             ?>We're sorry, but the page you requested could not be found.<br /><?php
         }
     }
